46aa668cba149b80425e43238770bd045e8fcbb1
unknown <devjonathanbrito@gmail.com>
2021-01-07 13:02:21 -0300

    front-web finalizado

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepesHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+        
+
+    }, []);
+
+
     return (
         <div className="orders-container">
             <StepesHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
