57c964a04b17be3b33c343b5f082ed54eac30599
unknown <devjonathanbrito@gmail.com>
2021-01-10 18:30:06 -0300

    Add Home pages

--- front-mobile/src/Home/index.tsx
+++ front-mobile/src/Home/index.tsx
@@ -3,23 +3,28 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
 function Home() {
-  return (
-    <>
-        <View style={styles.container}>
-            <Image source={require('../assets/deliveryman.png')} /> 
-            <Text style={styles.title}>
-                Acompanhe os pedidos e {'\n'} entregue no prazo!
-            </Text>
-            <Text style={styles.subTitle}>
-                Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
-            </Text>
-        </View>
-        <View style={styles.footer}>
-            <RectButton style={styles.button}>
-                <Text style={styles.buttonText}>VER PEDIDO</Text>
-            </RectButton>
-        </View>
-    </>
+
+    const handleOnPress = () => {
+
+    }
+
+    return (
+        <>
+            <View style={styles.container}>
+                <Image source={require('../assets/deliveryman.png')} /> 
+                <Text style={styles.title}>
+                    Acompanhe os pedidos e {'\n'} entregue no prazo!
+                </Text>
+                <Text style={styles.subTitle}>
+                    Receba todos os pedidos do seu {'\n'} restaurante na palma da sua mão
+                </Text>
+            </View>
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={handleOnPress}>
+                    <Text style={styles.buttonText}>VER PEDIDO</Text>
+                </RectButton>
+            </View>
+        </>
 
   );
 }
